b4f07b55b90fd7dd988d62f10bbde33225b24175
Rahma <rahmaosama2804@gmail.com>
2025-11-27 19:41:55 +0200

    add search & traversals of BST

--- src/BST/BST.java
+++ src/BST/BST.java
@@ -18,8 +18,64 @@ class BST {
         root = null;
     }
 
-   
+    // Search function
+    boolean search(int key) {
+        return searchRec(root, key);
+    }
+
+    private boolean searchRec(Node root, int key) {
+        if (root == null) {
+            return false; // key not found
+        }
+        if (root.key == key) {
+            return true; // key found
+        }
+        if (key < root.key) {
+            return searchRec(root.left, key);
+        } else {
+            return searchRec(root.right, key);
+        }
+    }
+
+    // Inorder traversal
+    void inorder() {
+        inorderRec(root);
+        System.out.println();
+    }
+
+    private void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.key + " ");
+            inorderRec(root.right);
+        }
+    }
+
+    // Preorder traversal
+    void preorder() {
+        preorderRec(root);
+        System.out.println();
+    }
+
+    private void preorderRec(Node root) {
+        if (root != null) {
+            System.out.print(root.key + " ");
+            preorderRec(root.left);
+            preorderRec(root.right);
+        }
+    }
+
+    // Postorder traversal
+    void postorder() {
+        postorderRec(root);
+        System.out.println();
+    }
+
+    private void postorderRec(Node root) {
+        if (root != null) {
+            postorderRec(root.left);
+            postorderRec(root.right);
+            System.out.print(root.key + " ");
+        }
+    }
 }
-
-
-
